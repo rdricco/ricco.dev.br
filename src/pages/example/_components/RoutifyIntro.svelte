99df8a02f894d582e52ffcb193710e073ca35f8e
Renato Ricco <renato.mur@gmail.com>
2020-08-05 01:08:21 -0300

    routify

---
+++ src/pages/example/_components/RoutifyIntro.svelte
@@ -0,0 +1,4 @@
+<h1>Routify Starter</h1>
+
+To see an example page, go to 
+<a href="/example">/example</a>
---
+++ src/pages/index.svelte
@@ -0,0 +1,21 @@
+<script>
+    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
+    import { metatags } from '@sveltech/routify'
+    metatags.title = 'My Routify app'
+    metatags.description = 'Description coming soon...'
+</script>
+
+<RoutifyIntro />
+<h1>
+    Renato Ricco
+</h1>
+
+<p>
+    Estudei Ciência da Computação, Comunicação Visual e atualmente curso Gestão em Tecnologia da Informação. Carrego comigo uma experiência bem ampla em outras áreas as quais já atuei, que abrangem desde a produção de eventos, rotinas administrativas e gerenciamento de pessoal à administração de redes e automação de processos.
+<br>    
+<br>
+    Frequentemente busco ampliar meu conhecimento e repertório através de cursos e oficinas que agreguem além dos campos que atuo profissionalmente. Apaixonado por tecnologia, busco me manter atualizado sobre o que há de mais inovador a minha disposição e quais são as novas tendências da area. Como entusiasta de software livre, sempre contribuo em projetos abertos sugerindo correções de bugs, melhorias na usabilidade ou fornecendo feedbacks gerais. 
+    <br>
+    <br>
+    Atualmente meus projetos pessoais são baseados em JAMStack e Content Driven Development. 
+</p>
