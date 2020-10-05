b117513083a4270d5e5b96b9c1ebb5b377ae0f04
Renato Ricco <renato@ricco.dev.br>
2020-10-05 01:03:12 -0300

    Commento

---
+++ src/pages/blog/_commento.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+  <script src="https://commento.r-cc.xyz/js/commento.js"></script>
+</svelte:head>
+
+<div id="commento" />
--- src/pages/blog/_layout.svelte
+++ src/pages/blog/_layout.svelte
@@ -1,10 +1,8 @@
-<!-- routify:options index=20 -->
-<slot />
-<script src="https://utteranc.es/client.js"
-        repo="rdricco/jamstack-comments"
-        issue-term="pathname"
-        label="comments"
-        theme="github-light"
-        crossorigin="anonymous"
-        async>
+<script>
+	import Commento from "./_commento.svelte";
 </script>
+
+<slot />
+<!-- routify:options index=20 -->
+
+<Commento/>
